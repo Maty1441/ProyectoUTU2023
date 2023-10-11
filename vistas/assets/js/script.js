740ac4d6da5e09df0dc2ec51c1e76497e08d1244
Maty1441 <mathiasls610@gmail.com>
2023-10-11 20:13:55 -0300

    pagina puntuacion hecha

---
+++ vistas/assets/js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const inputField = document.getElementById("inputField");
+    const sumarButton = document.getElementById("sumar");
+    const restarButton = document.getElementById("restar");
+    const descalificarLink = document.getElementById("descalificar");
+
+    let descalificado = false; // Variable de estado
+
+    sumarButton.addEventListener("click", function () {
+        if (!descalificado) {
+            let valor = parseFloat(inputField.value);
+            valor += 0.1;
+            if (valor <= 10.0) {
+                inputField.value = valor.toFixed(1);
+            }
+        }
+    });
+
+    restarButton.addEventListener("click", function () {
+        if (!descalificado) {
+            let valor = parseFloat(inputField.value);
+            valor -= 0.1;
+            if (valor >= 5.0) {
+                inputField.value = valor.toFixed(1);
+            }
+        }
+    });
+
+    inputField.addEventListener("input", function () {
+        let valor = parseFloat(inputField.value);
+        if (valor < 5.0) {
+            inputField.value = "5.0";
+        }
+        if (valor > 10.0) {
+            inputField.value = "10.0";
+        }
+    });
+
+    // Función para descalificar
+    descalificarLink.addEventListener("click", function () {
+        inputField.value = "0.0";
+        descalificado = true; // Establece la variable de estado como verdadera
+    });
+});
